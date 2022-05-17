41c7f8d18feda97236a7362877d8b79d0f250827
savchuckvadim <savchuckvadim@gmail.com>
2022-05-17 07:38:05 +0300

    fixed start-page

--- src/App-Container.js
+++ src/App-Container.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { compose } from "redux";
 import App from "./App";
 import { LightLoadingPageContainer } from "./modules/components/Elements/Loading/Light-Loading-Page-Container";
+import LoginContainer from "./modules/components/Elements/Login-Registaration-Form/Login-Container";
 import withAuthRedirect from "./modules/components/HOC/Auth-Redirect";
 import StartPage from "./modules/components/Start/Start-Page";
 import { initialize } from "./modules/redux/reducers/app-reducer";
@@ -28,7 +30,15 @@ class AppContainer extends React.Component {
     render() {
        debugger
         if(!this.props.isAuth && !this.props.initialInProgress){
-            return <StartPage/>
+            return (<>
+            {/* <Routes >
+                <Route path="/" index element={<StartPage />} />
+                <Route path="login" element={<LoginContainer />} />
+            </Routes> */}
+            {/* <Navigate replace to='start' /> */}
+             <StartPage/>
+             </>
+             )
         }
         if(this.props.initialInProgress){
             return <LightLoadingPageContainer/>
