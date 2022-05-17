c09ad8da62b4f5cc7cbda45cca0dec0f0a589d11
savchuckvadim <savchuckvadim@gmail.com>
2022-05-17 14:45:26 +0300

    login page layout

--- src/modules/components/Login-Page/Login-Page.jsx
+++ src/modules/components/Login-Page/Login-Page.jsx
@@ -2,15 +2,23 @@ import style from './Login-Page.module.css'
 import LoginContainer from './Login-Registaration-Form/Login-Container'
 import greyLogo from '../../../assets/imgs/grey-logo.svg'
 import redLogo from '../../../assets/imgs/logo.svg'
+import HeaderContainer from '../../components/Header/Header-Container';
 const LoginPage = (props) => {
 
     return (
-        <div className={style.page}>
+       
+        <>
+        <div className={style.header}>
+            {/* <HeaderContainer /> */}
+            </div>
+        <div className={style.page__container}>
+            
             <div className={style.form__wrapper}>
                 <LoginContainer />
             </div>
-            <img className={style.greyLogo} src={redLogo} alt='img-logo' />
+            <img className={style.greyLogo} src={greyLogo} alt='img-logo' />
         </div>
+        </>
     )
 }
 
