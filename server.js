1c197798d90bf6621b9294413e45c5690e339999
savchuckvadim <savchuckvadim@gmail.com>
2022-08-04 16:17:22 +0300

    testing deployment

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, 'build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-app.listen(PORT)
+});
+app.listen(PORT);
