ab8623ccb5d3e2f3fc37f4dfe2b89c947efe7901
savchuckvadim <savchuckvadim@gmail.com>
2022-09-01 16:40:27 +0300

    add websocket service

--- src/modules/services/websocket/socket.js
+++ src/modules/services/websocket/socket.js
@@ -8,3 +8,11 @@ window.Echo = new Echo({
     cluster: 'mt1',
     forceTLS: true
 });
+
+//prefences from laravel-websocket 
+// broadcaster: 'pusher',
+// key: 'your-pusher-key',
+// wsHost: window.location.hostname,
+// wsPort: 6001,
+// forceTLS: false,
+// disableStats: true,
